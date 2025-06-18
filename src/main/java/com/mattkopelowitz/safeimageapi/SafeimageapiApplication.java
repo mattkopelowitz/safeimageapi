135ce92f622c925f2ea6d325359fd522dc6cda7d
MatthewKopelowitz <47190077+mattkopelowitz@users.noreply.github.com>
2025-06-18 12:23:44 -0400

    .env

--- src/main/java/com/mattkopelowitz/safeimageapi/SafeimageapiApplication.java
+++ src/main/java/com/mattkopelowitz/safeimageapi/SafeimageapiApplication.java
@@ -2,11 +2,14 @@ package com.mattkopelowitz.safeimageapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class SafeimageapiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(SafeimageapiApplication.class, args);
 	}
 
